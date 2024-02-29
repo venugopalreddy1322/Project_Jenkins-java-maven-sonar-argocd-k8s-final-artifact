6d66dac91616a11bc38ab38102fb18b4f5f34086
venugopalreddy1322 <venugopalreddy1322@gmail.com>
2024-02-29 22:59:19 +0100

    changes

--- src/main/java/com/venu/StartApplication.java
+++ src/main/java/com/venu/StartApplication.java
@@ -12,8 +12,8 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "Sprint boot application");
-        model.addAttribute("msg", "CI: JENKINS:application is packaged using Maven, test by SonarQube and stored articat to another Git reporitory, CD:ArgoCD: deployed on to Kubernetes using ArgoCD operator ");
+        model.addAttribute("title", "I have successfuly built a spring boot application using Maven");
+        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
         return "index";
     }
 
